d6c92526ed44cb73cae1d15ea0ffa6314f14e032
aviraltrip <aviraltripathi25@gmail.com>
2026-05-12 02:07:02 +0530

    added readme

---
+++ README.md
@@ -0,0 +1,3 @@
+# Learning Web Scraping
+
+A simple Python web scraper that fetches and parses HTML content using `requests` and `BeautifulSoup`.
--- scraper.py
+++ scraper.py
@@ -1,5 +1,5 @@
 from bs4 import BeautifulSoup
 import requests
 
-html_text=requests.get('https://books.toscrape.com/')
-print(html_text)
+html_text=requests.get('https://books.toscrape.com/').text
+soup=BeautifulSoup(html_text, 'lxml')
