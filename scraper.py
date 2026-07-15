3969140528a9a3a985ee9ee06a48b1b6371165c5
aviraltrip <aviraltripathi25@gmail.com>
2026-07-16 02:23:05 +0530

    feat: update scraper to extract and display book prices alongside titles

--- scraper.py
+++ scraper.py
@@ -6,4 +6,8 @@ soup=BeautifulSoup(html_text, 'lxml')
 books = soup.find_all('article', class_='product_pod')
 for book in books:
     book_name = book.find('h3').a['title']
-    print(book_name)
+    price = book.find('p', class_='price_color').text.strip()
+    print(f'''
+Book Name: {book_name}
+Price: {price}
+''')
