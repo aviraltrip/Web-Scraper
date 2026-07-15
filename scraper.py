dd3bfc5d1887ffbbcba8d66622ae79296e4cbbee
aviraltrip <aviraltripathi25@gmail.com>
2026-07-16 02:21:05 +0530

    feat: extract and print book titles from the scraped HTML content

--- scraper.py
+++ scraper.py
@@ -3,3 +3,7 @@ import requests
 
 html_text=requests.get('https://books.toscrape.com/').text
 soup=BeautifulSoup(html_text, 'lxml')
+books = soup.find_all('article', class_='product_pod')
+for book in books:
+    book_name = book.find('h3').a['title']
+    print(book_name)
