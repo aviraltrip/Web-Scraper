5d4b96cb18741765e1a17b107b02b27e3bd419b2
aviraltrip <aviraltripathi25@gmail.com>
2026-07-16 19:35:17 +0530

    feat: implement Amazon laptop scraper with paginated HTML data collection

--- selenium/locating_single.py
+++ selenium/locating_single.py
@@ -7,12 +7,12 @@ import time
 
 driver = webdriver.Chrome()
 query="laptop"
-driver.get(f"https://www.amazon.in/s?k={query}&crid=2RKOOCZHIKESC&sprefix=lapt%2Caps%2C337&ref=nb_sb_noss_2")
-
-elems = driver.find_elements(By.CLASS_NAME, "a-price")
-print(f"{len(elems)} items found")
-for elem in elems:
-    print(elem.text)
-#print(elem.get_attribute("outerHTML"))
-time.sleep(5)
-driver.close()
+for i in range (1,20):
+    driver.get(f"https://www.amazon.in/s?k={query}&crid=2RKOOCZHIKESC&sprefix=lapt%2Caps%2C337&ref=nb_sb_noss_2")
+    elems = driver.find_elements(By.CLASS_NAME, "a-price")
+    print(f"{len(elems)} items found")
+    for elem in elems:
+        print(elem.text)
+        #print(elem.get_attribute("outerHTML"))
+        time.sleep(5)
+        driver.close()
---
+++ selenium/project.py
@@ -0,0 +1,32 @@
+import sys
+import os
+sys.stdout.reconfigure(encoding='utf-8')
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+
+os.makedirs("data", exist_ok=True)
+
+driver = webdriver.Chrome()
+query = "laptop"
+file = 0
+
+for i in range(1, 20):
+    driver.get(f"https://www.amazon.in/s?k={query}&page={i}&crid=2RKOOCZHIKESC&sprefix=lapt%2Caps%2C337&ref=nb_sb_noss_2")
+    elems = driver.find_elements(By.CLASS_NAME, "a-price")
+    print(f"Page {i}: {len(elems)} items found")
+
+    for elem in elems:
+        try:
+            d = elem.get_attribute("outerHTML")
+            with open(f"data/{query}_{file}", "w", encoding="utf-8") as f:
+                f.write(d)
+            print(f"file saved: {query}_{file}")
+            file += 1
+        except Exception as e:
+            print(f"Error reading element: {e}")
+            
+    time.sleep(2)  
+
+driver.quit()
